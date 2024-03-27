8416d721a242ad215277ae4770b939324783bb47
OlgaDonchenko <n0931606045@gmail.com>
2024-03-27 15:52:59 +0200

    in process

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,27 @@
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
+import Profile from "../components/Profile";
 
 export default function App() {
-  return (
-    <div>
-      <div>
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-        />
-        <p>Petra Marica</p>
-        <p>@pmarica</p>
-        <p>Salvador, Brasil</p>
-      </div>
+  const userData = {
+    username: "Jacques Gluke",
+    tag: "jgluke",
+    location: "Ocho Rios, Jamaica",
+    avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+    stats: {
+      followers: 5603,
+      views: 4827,
+      likes: 1308,
+    },
+  };
 
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>1000</span>
-        </li>
-        <li>
-          <span>Views</span>
-          <span>2000</span>
-        </li>
-        <li>
-          <span>Likes</span>
-          <span>3000</span>
-        </li>
-      </ul>
-    </div>
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
   );
 }
