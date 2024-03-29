72a15bac84d429910d3e8558b442e56cbe29271b
OlgaDonchenko <n0931606045@gmail.com>
2024-03-29 16:20:05 +0200

    added css to 3 task

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,20 +1,20 @@
 import css from "./TransactionHistory.module.css";
 export default function TransactionHistory({ transactions }) {
   return (
-    <table>
+    <table className={css.table}>
       <thead>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={css.thUp}>Type</th>
+          <th className={css.thUp}>Amount</th>
+          <th className={css.thUp}>Currency</th>
         </tr>
       </thead>
       <tbody>
         {transactions.map(({ id, type, amount, currency }) => (
-          <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+          <tr key={id} className={css.lineTr}>
+            <td className={css.lineTd}>{type}</td>
+            <td className={css.lineTd}>{amount}</td>
+            <td className={css.lineTd}>{currency}</td>
           </tr>
         ))}
       </tbody>
